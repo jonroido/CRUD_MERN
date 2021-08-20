d2a538b3e6d35656292d14d2fc6f6de469df4d2f
jon roido <jlgt2200@gmail.com>
2021-08-21 06:01:17 +0800

    post one, get one and get all, put and delete api

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,65 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//get all
+app.get("/", (req, res) => {
+  Todo.find((err, todos) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+
+//get one
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Todo.findById(id, (err, todo) => {
+    res.json(todo);
+  });
+});
+
+//create one
+app.post("/", (req, res) => {
+  const todo = new Todo(req.body);
+  todo
+    .save()
+    .then((todo) => {
+      res.json(todo);
+    })
+    .catch((err) => {
+      res.status(500).send(err.message);
+    });
+});
+
+//put one
+app.put("/:id", (req, res) => {
+  const id = req.params.id;
+  Todo.findById(id, (err, todo) => {
+    if (!todo) {
+      res.status(404).send("Todo not found");
+    } else {
+      todo.text = req.body.text;
+
+      todo
+        .save()
+        .then((todo) => {
+          res.json(todo);
+        })
+        .catch((err) => res.status(500).send(err.message));
+    }
+  });
+});
+
+//delete one 
+app.delete("/:id",  (req, res) => {
+  Todo.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Todo deleted."))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
