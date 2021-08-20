0a4de31ccb55c7332e9ded6343dad242b3b993a7
jon roido <jlgt2200@gmail.com>
2021-08-21 06:58:57 +0800

    editTodo connected API, edit function

--- client/src/api.js
+++ client/src/api.js
@@ -6,5 +6,13 @@ export const createTodo = (todo) => fetch("http://localhost:4000/", {
   },
   body: JSON.stringify(todo)
 }) 
-
 export const getTodos = () => fetch("http://localhost:4000/").then(res => res.json())
+export const updateTodo = (todo, id) => fetch(`http://localhost:4000/${id}`, {
+  method: "POST",
+  headers: {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(todo)
+})  
+export const getTodo = (id) => fetch(`http://localhost:4000/${id}`).then(res => res.json())
